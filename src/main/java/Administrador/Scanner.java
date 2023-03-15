3dc12969ed0bc3647a898ed4bf6d84f009fc16c2
alannicolasflores <alan.sallan@hotmail.com>
2023-03-14 22:55:12 -0600

    C3

--- src/main/java/Administrador/Scanner.java
+++ src/main/java/Administrador/Scanner.java
@@ -77,55 +77,69 @@ public class Scanner {
 
     Scanner(String source){
         this.source = source;
-        //int i = source.length();
+
         
     }
 
     List<Token> scanTokens(){
         //Aquí va el corazón del scanner.
-        int i= 0;
         int j=source.length();
-        while (source.charAt(i)!= j ) {
-            
-        }
-        if("a"== source.substring(1)){
+        // while (source.charAt(linea)!= j ) {
+        //     if("a"== source.substring(linea-1)){
            
-            if("and"==source.substring(1, 3)){
-                tokens.add(new Token(TipoToken.AND, "and",palabrasReservadas, linea++));
-                i=i+3;
-            }
-            if("also"==source.substring(1, 4)){
-                tokens.add(new Token(TipoToken.ALSO, "also",palabrasReservadas, linea++));
-                i=i+4;
-            } 
-        }
-        if ("class"== source.substring(1, 5)) {
-            tokens.add(new Token(TipoToken.CLASS, "class", palabrasReservadas, linea++));
-            i=i+5;
-        }
-
-               
-        if ("(" == source) {
-            tokens.add(new Token(TipoToken.PAR1, "(",simbolos, linea++));    
-        }
-        if (")" == source) {
-            tokens.add(new Token(TipoToken.PAR2, ")",simbolos, linea++));    
-        }
-        if("{"==source){
-            tokens.add(new Token(TipoToken.LLAVE1, "{",simbolos, linea++));
-        }
-        if("}"==source){
-            tokens.add(new Token(TipoToken.LLAVE2, "}",simbolos, linea++));
-        }
-        if(","==source){
-            tokens.add(new Token(TipoToken.COMA, ",",simbolos, linea++));
-        }        
-        if("."==source){
-            tokens.add(new Token(TipoToken.PUNTO, ".",simbolos, linea++));
-        }
-        if("}"==source){
-            tokens.add(new Token(TipoToken.LLAVE2, "}",simbolos, linea++));
-        }
+        //         if("and"==source.substring(linea-1, linea+2)){
+        //             tokens.add(new Token(TipoToken.AND, "and",palabrasReservadas, linea));
+        //             linea=linea+3;
+        //         }
+        //         if("also"==source.substring(linea-1, linea+3)){
+        //             tokens.add(new Token(TipoToken.ALSO, "also",palabrasReservadas, linea));
+        //             linea=linea+4;
+        //         } 
+        //     }
+        //     if ("class"== source.substring(linea-1,linea+4)) {
+        //         tokens.add(new Token(TipoToken.CLASS, "class", palabrasReservadas, linea));
+        //         linea=linea+5;
+        //     }
+            
+        //     if("f"== source.substring(linea-1))
+        //         if ("false"== source.substring(linea-1, linea+4)) {
+        //             tokens.add(new Token(TipoToken.FALSE, "class", palabrasReservadas, linea));
+        //             linea=linea+5;
+        //         }else if("fun"== source.substring(linea-1, linea+2)){
+        //             tokens.add(new Token(TipoToken.FUN, "class", palabrasReservadas, linea));
+        //             linea=linea+3;
+        //         }
+             
+                   
+        //     if ("(" == source.substring(linea-1)) {
+        //         tokens.add(new Token(TipoToken.PAR1, "(",simbolos, linea++));    
+        //     }
+        //     if (")" == source.substring(linea-1)) {
+        //         tokens.add(new Token(TipoToken.PAR2, ")",simbolos, linea++));    
+        //     }
+        //     if("{"==source.substring(linea-1)){
+        //         tokens.add(new Token(TipoToken.LLAVE1, "{",simbolos, linea++));
+        //     }
+        //     if("}"==source.substring(linea-1)){
+        //         tokens.add(new Token(TipoToken.LLAVE2, "}",simbolos, linea++));
+        //     }
+        //     if(","==source.substring(linea-1)){
+        //         tokens.add(new Token(TipoToken.COMA, ",",simbolos, linea++));
+        //     }        
+        //     if("."==source.substring(linea-1)){
+        //         tokens.add(new Token(TipoToken.PUNTO, ".",simbolos, linea++));
+        //     }
+        //     if("}"==source.substring(linea-1)){
+                 tokens.add(new Token(TipoToken.LLAVE2, "}",simbolos, linea++));
+        //     }
+            
+            
+            
+        
+        
+        
+        // }
+        
 
         /*
         Analizar el texto de entrada para extraer todos los tokens
